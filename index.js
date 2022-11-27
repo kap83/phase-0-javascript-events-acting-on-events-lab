5adbcd87845546ae04082698ba50bc883429e360
Karen Polanco <polanco.k@gmail.com>
2022-11-27 14:27:51 -0500

    commit tree 2bf6f1db42aef5b8b25913009237cd7ce4d6ece5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+// moves the DODGER to the left:
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+
+  //moves the DODGER to the right:
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
